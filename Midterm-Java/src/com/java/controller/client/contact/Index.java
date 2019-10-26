37e5ab8cd74712e29121899d0501a73edd29b5be
Thanhhv317 <thanhhoang317@gmail.com>
2019-10-27 00:11:51 +0700

    complete contact page: send feedback for customer

--- Midterm-Java/src/com/java/controller/client/contact/Index.java
+++ Midterm-Java/src/com/java/controller/client/contact/Index.java
@@ -1,6 +1,7 @@
 package com.java.controller.client.contact;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.java.common.ProductCategory;
 import com.java.model.CategoryModel;
+import com.java.model.ConnectDB;
+import com.java.model.Contact;
 
 
 @WebServlet("/Contact")
@@ -24,6 +27,34 @@ public class Index extends HttpServlet {
 		array = p.GetListCate();
 		req.setAttribute("category", array);
 		req.getRequestDispatcher("view/client/contact.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html;charset=UTF-8");
+		req.setCharacterEncoding("utf-8");
+		PrintWriter p = resp.getWriter();
+		
+		Contact contact = new Contact();
+		
+		contact.name = req.getParameter("name");
+		contact.email = req.getParameter("email");
+		contact.subject = req.getParameter("subject");
+		contact.message = req.getParameter("message");
+		
+		ConnectDB conn = new ConnectDB();
+		
+		boolean check = conn.updateData(contact.add());
+		if(check) {
+			p.println("<script>alert('da gui');</script>");
+		}
+		else {
+			p.println("<script>alert('Loi');</script>");
+		}
+		
+		
+		
 	}
 
 }
